038af8f68d4f4ea481ec2edb9cbf36c242038455
LeoDumas <leo.ndumas980@gmail.com>
2023-12-02 21:22:53 +0100

    fix : shopAll component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import HomePage from "./pages/HomePage"
+import HomePage from "./pages/HomePage";
 import ShopAll from "./pages/ShopAll";
+import items from "./assets/products.json";  // Import or fetch your item data
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/clothes-store/" element={<HomePage />} />
-				<Route path="/clothes-store/shop-all" element={<ShopAll/>}></Route>
-			</Routes>	
-		</BrowserRouter>
-	)
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/clothes-store/" element={<HomePage />} />
+        <Route path="/clothes-store/shop-all" element={<ShopAll items={items.items} />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
